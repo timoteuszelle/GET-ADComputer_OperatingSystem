240e9f093e1183d428fd66dd23a53e3f6701638f
timoteuszelle <46320568+timoteuszelle@users.noreply.github.com>
2020-01-20 14:27:47 +0100

    Update FindOsVersions.ps1

--- FindOsVersions.ps1
+++ FindOsVersions.ps1
@@ -2,7 +2,7 @@
 #Windows 8
 #Windows Server 2016
 #Windows 10
-#could make it fancy but not got to pactch right now..
+#could make it fancy but not got to patch right now..
 
 $computer = Get-ADComputer -properties OperatingSystem -filter 'operatingsystem -like "*Windows Server 2019*" -or operatingsystem -like "*Windows 8*" -or operatingsystem -like "*Windows 10*" -or operatingsystem -like "*Windows Server 2016*"' 
 $computer | Select-Object Name, OperatingSystem
