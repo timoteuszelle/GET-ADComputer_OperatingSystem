e6afcd5950e8e30bd58a6e4b55fd5aa407f65d47
timoteuszelle <46320568+timoteuszelle@users.noreply.github.com>
2020-01-22 14:56:18 +0100

    Update FindOsVersions.ps1

--- FindOsVersions.ps1
+++ FindOsVersions.ps1
@@ -1,8 +1,11 @@
-#Windows Server 2019
-#Windows 8
-#Windows Server 2016
-#Windows 10
-#could make it fancy but not got to patch right now..
-
+<#
+#Get the AD Computer objects with the specified OS: 
+#   Windows Server 2019
+#   Windows Server 2016
+#   Windows 10
+#   Windows 8
+#   "Whatever other version you like to find"
+# -Filter 'operatingsystem -like "*Fill in the OS between the astrix*" -or' (use the or operator for more than one OS type)
+#>
 $computer = Get-ADComputer -properties OperatingSystem -filter 'operatingsystem -like "*Windows Server 2019*" -or operatingsystem -like "*Windows 8*" -or operatingsystem -like "*Windows 10*" -or operatingsystem -like "*Windows Server 2016*"' 
 $computer | Select-Object Name, OperatingSystem
