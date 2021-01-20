3534e8f991ccbbe220f1a160d869b06dfa6a97dd
Tim Zelle <tim.zelle@sublie.me>
2021-01-20 12:36:07 +0100

    updated logic

--- FindOsVersions.ps1
+++ FindOsVersions.ps1
@@ -1,11 +1,14 @@
 <#
-#Get the AD Computer objects with the specified OS: 
+Author: Tim Zelle
+--
+# Get the AD Computer objects with the specified OS: 
 #   Windows Server 2019
 #   Windows Server 2016
 #   Windows 10
-#   Windows 8
-#   "Whatever other version you like to find"
+#   Windows 8 and 7 etc.
+#   "Whatever other version you like to find, in the example use -notlike operator to find anything else than windows."
 # -Filter 'operatingsystem -like "*Fill in the OS between the astrix*" -or' (use the or operator for more than one OS type)
 #>
-$computer = Get-ADComputer -properties OperatingSystem -filter 'operatingsystem -like "*Windows Server 2019*" -or operatingsystem -like "*Windows 8*" -or operatingsystem -like "*Windows 10*" -or operatingsystem -like "*Windows Server 2016*"' 
-$computer | Select-Object Name, OperatingSystem
+$computerw = Get-ADComputer -properties OperatingSystem -filter 'operatingsystem -like "*windows*"'
+$computero = Get-ADComputer -properties OperatingSystem -filter 'operatingsystem -notlike "*windows*"'
+$computerw + $computero | Select-Object Name, OperatingSystem >c:\temp\computerobjectsOStypes.txt
